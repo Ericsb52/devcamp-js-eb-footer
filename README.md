d2a6114f6a6078b2b78da49e4169da077d334610
Eric <Broadbent52@aol.com>
2018-06-28 09:54:47 -0600

    first commit 6/26/2018

---
+++ README.md
@@ -0,0 +1,17 @@
+## Devcamp js Footer
+
+> this should be used in the folowing manner 
+
+install with the comand:
+
+```
+npm install --save devcamp-js-eb-footer
+```
+Add to a javaScript project with the following code:
+
+```javascript
+import{ footer } form 'devcamp-js-eb-footer';
+
+footer( 'Some Name');
+```
+
---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var moment = require('moment');
+
+/**
+ * returns a string element with a footer and updating year
+ * @param {string} name
+ * @return {string}
+ */
+
+ exports.footer = function(name){
+     return "Gopyright " + moment().format('YYYY') + " " + name + " All rights reserved";
+ };
